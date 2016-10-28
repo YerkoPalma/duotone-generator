7ed607767195dcfef73cbac96f980a3473edc110
Yerko Palma <yerko.palma@usach.cl>
2016-10-28 17:51:27 +0000

    started tests

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const execa = require('execa')
 const Confirm = require('prompt-confirm')
 const path = require('path')
 const fs = require('fs')
+const assert = require('assert')
 // const style = require('ansi-styles')
 
 // minimum contrast constants
@@ -21,14 +22,21 @@ const gen = (hueUno, hueDuo, options) => {
 
 const check = (hueUno, hueDuo, options) => {
   options = options || {}
-  const minimum = options.minimum 
-                  ? (options.minimum === 'AA' || options.minimum === 'AAA' ? minimums[options.minimum] : minimums['AA']) 
+  assert.ok(hueUno, '[duo check]: hueUno not provided')
+  assert.ok(hueDuo, '[duo check]: hueDuo not provided')
+  assert.equal(typeof hueUno, 'number', '[duo check]: hueUno must be a number')
+  assert.equal(typeof hueDuo, 'number', '[duo check]: hueDuo must be a number')
+  assert(hueUno > 0 && hueUno <= 360, '[duo check]: Hue values must be between 0 and 360')
+  assert(hueDuo > 0 && hueDuo <= 360, '[duo check]: Hue values must be between 0 and 360')
+
+  const minimum = options.minimum
+                  ? (options.minimum === 'AA' || options.minimum === 'AAA' ? minimums[options.minimum] : minimums['AA'])
                   : minimums['AA']
 
   const colorUno = chroma.hsl(hueUno, 0.99, 0.96)
   const colorDuo = chroma.hsl(hueDuo, 0.99, 0.77)
   const bgColor = chroma.hsl(hueUno, 0.12, 0.18)
-  
+
   const contrastUno = chroma.contrast(colorUno, bgColor)
   const contrastDuo = chroma.contrast(colorDuo, bgColor)
   // const errorColor = chroma('pink').rgb
@@ -44,27 +52,28 @@ const check = (hueUno, hueDuo, options) => {
       message: 'Your colors are fine, would you like to generate a duotone theme now?'
     })
 
-    confirmGeneration.ask(function (answer) {
+    confirmGeneration.ask(answer => {
       if (!answer) process.exit(0)
 
+      // duo gen::
       execa('git', ['clone', REPO]).then(result => {
         // the colors.less file from the original repo
         const colors = path.resolve(__dirname, path.join(themeName, 'styles', 'colors.less'))
 
-        fs.readFile(colors, 'utf8', function (err,data) {
+        fs.readFile(colors, 'utf8', (err, data) => {
           if (err) {
             return console.log(err)
           }
-          let result = data.replace(/@syntax-uno:    240;/g, `@syntax-uno:    ${hueUno};`)
-          result = result.replace(/@syntax-duo:    20;/g, `@syntax-duo:    ${hueDuo};`)
-        
-          fs.writeFile(colors, result, 'utf8', function (err) {
-             if (err) return console.log(err)
+          let result = data.replace(/@syntax-uno:\s*240;/g, `@syntax-uno:    ${hueUno};`)
+          result = result.replace(/@syntax-duo:\s*20;/g, `@syntax-duo:    ${hueDuo};`)
+
+          fs.writeFile(colors, result, 'utf8', err => {
+            if (err) return console.log(err)
           })
         })
       })
+      // ::duo gen
     })
-    // console.log(`${style.color.ansi256.grb(...successColor)} Nice contrast! ${style.color.close}`)
   }
 }
 
--- test.js
+++ test.js
@@ -1,8 +1,36 @@
 import test from 'ava'
 import duotoneGenerator from './'
 
-test('duotone-generator does something awesome', t => {
-  t.plan(1)
+test('duo check', t => {
+  t.plan(5)
 
-  t.true(duotoneGenerator())
+  // test arguments
+  // check should throw if there is less or more than two parameters
+  t.throws(() => { duotoneGenerator.check() })
+  t.throws(() => { duotoneGenerator.check(45) })
+  // parameters must be integers between 0 and 360
+  t.throws(() => { duotoneGenerator.check('56', 'black') })
+  t.throws(() => { duotoneGenerator.check(10, 500) })
+
+  // default minimum should be AA
+  // opts can change minimum
+  // low contrast do not allow to generate theme
+  // after cloning repo, there must be a folder with the name of the project
+  t.notThrows(() => {
+    duotoneGenerator.check(10, 300)
+  })
+  // theme/styles/colors.less must have user hues
+})
+
+test('duo gen', t => {
+  // test arguments
+  // gen should work 0 to 2 arguments
+  // if no arguments, two hues are generated with maximum contrast difference
+  // if one argument, a second hue is generated with maximum contrast difference
+
+  // default minimum should be AA
+  // opts can change minimum
+  // opts can change theme name
+  // after cloning repo, there must be a folder with the name of the project
+  // theme/styles/colors.less must have user hues
 })
